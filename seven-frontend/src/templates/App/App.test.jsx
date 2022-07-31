5f7421efe31b622d577c8e4f79d2f090e62f9af3
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-07-31 12:18:18 -0300

    [add] snapchot to App template

--- seven-frontend/src/templates/App/App.test.jsx
+++ seven-frontend/src/templates/App/App.test.jsx
@@ -5,4 +5,10 @@ test('renders learn react link', () => {
   const { debug } = render(<App />);
   debug();
   const headingContainer = screen.getByRole('heading', {name: 'Home'}).parentElement;
+  expect(headingContainer).toHaveStyle({
+    background: 'red'
+  });
+
+  expect(headingContainer).toMatchSnapshot();
+
 });
