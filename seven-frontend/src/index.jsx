ca3f9f6211bb871c56418e207813280c4c7f82e2
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-07-31 21:15:30 -0300

    [add] importing fonts

--- seven-frontend/src/index.jsx
+++ seven-frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
+import styled, { css, ThemeProvider } from 'styled-components';
 import './index.css';
 
 import { theme } from './styles/theme';
@@ -8,9 +8,13 @@ import GlobalStyles from './styles/global-styles';
 import App from './templates/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
+    <ThemeProvider theme={theme}>
       <App />
-      <GlobalStyles/>   
+      <GlobalStyles/> 
+    </ThemeProvider>
+             
   </React.StrictMode>
 );
--- seven-frontend/src/styles/global-styles.js
+++ seven-frontend/src/styles/global-styles.js
@@ -1,13 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Fjalla+One&family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Oswald:wght@400;500;600;700&family=Pacifico&family=Permanent+Marker&family=Poppins:wght@200;300;400;500;600&family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,700&family=Secular+One&display=swap');
     * {
         margin:0;
         padding:0;
     }
 
+    html {
+        font-size: 62.5%
+    }
+
     body {
-        background: yellow;
+        font-size: 1.6rem;
+        color: ${({ theme }) => theme.colors.white};
+        font-family: ${({theme}) => theme.font.family.secondary}
     }
 `
 
